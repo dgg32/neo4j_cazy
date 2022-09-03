39e4bf66c6cf27806dc3bb9d80b30de31983b113
Sixing Huang <dgg32@users.noreply.github.com>
2022-09-03 12:13:07 +0800

    Update download_cazy_genome.py

--- crawler/download_cazy_genome.py
+++ crawler/download_cazy_genome.py
@@ -2,7 +2,7 @@ import sys, re
 import requests
 from threading import Thread
 import queue
-import pyphy
+from pyphy import pyphy
 import json
 
 from threading import Semaphore
@@ -37,42 +37,41 @@ def work():
         container["cazy"] = {}
         container["taxonomy"] = {}
         
-        try:
-            container["name"] = re.findall(r'id="font_org">(.+)</font>', content)[0]
-            
-                    #container["organism"] = re.findall(r'id="font_org">(.+)</font>', content)[0]
-            cazy_page_taxon_name = re.findall(r'<font class="titre_cazome" id="font_org">(.+)<\/font>', content)[0]
-            #print (cazy_page_taxon_name)
-            taxonomy_id = -1
-            if cazy_page_taxon_name:
-                taxonomy_id = int(pyphy.getTaxidByName(cazy_page_taxon_name.strip())[0])
+        #try:
+        container["name"] = re.findall(r'id="font_org">(.+)</font>', content)[0]
+        
+                #container["organism"] = re.findall(r'id="font_org">(.+)</font>', content)[0]
+        cazy_page_taxon_name = re.findall(r'<font class="titre_cazome" id="font_org">(.+)<\/font>', content)[0]
+        #print (cazy_page_taxon_name)
+        taxonomy_id = -1
+        if cazy_page_taxon_name:
+            taxonomy_id = int(pyphy.getTaxidByName(cazy_page_taxon_name.strip())[0])
 
-            if taxonomy_id == -1:
-                taxonomy_id = re.findall(r'http://www\.ncbi\.nlm\.nih\.gov/Taxonomy/Browser/wwwtax\.cgi\?id=(\d+)', content)[0]
-            
-            container["taxid"] = int(taxonomy_id)
-        #lineage = re.findall(r'<b>Lineage</b>\:(.+)<br><br />', content)[0].strip()
+        if taxonomy_id == -1:
+            taxonomy_id = re.findall(r'http://www\.ncbi\.nlm\.nih\.gov/Taxonomy/Browser/wwwtax\.cgi\?id=(\d+)', content)[0]
         
-            current_id = int(taxonomy_id)
-            while current_id != 1 and current_id != -1:
-                current_id = int(pyphy.getParentByTaxid(current_id))
-                if pyphy.getRankByTaxid(current_id) in desired_ranks:
-                    container["taxonomy"][pyphy.getRankByTaxid(current_id)] = [pyphy.getNameByTaxid(current_id), current_id]
+        container["taxid"] = int(taxonomy_id)
+    #lineage = re.findall(r'<b>Lineage</b>\:(.+)<br><br />', content)[0].strip()
+    
+        current_id = int(taxonomy_id)
+        while current_id != 1 and current_id != -1:
+            current_id = int(pyphy.getParentByTaxid(current_id))
+            if pyphy.getRankByTaxid(current_id) in desired_ranks:
+                container["taxonomy"][pyphy.getRankByTaxid(current_id)] = [pyphy.getNameByTaxid(current_id), current_id]
+    
+        #container["total_gi"] = len(pyphy.getGiByTaxid(taxonomy_id))
+        cazies = re.findall(rx,content)
+    #print cazy
+        for cazy in cazies:
+            container["cazy"][cazy[0]] = int(cazy[1])
         
-            #container["total_gi"] = len(pyphy.getGiByTaxid(taxonomy_id))
-            cazies = re.findall(rx,content)
-        #print cazy
-            for cazy in cazies:
-                container["cazy"][cazy[0]] = int(cazy[1])
+        if len(container["cazy"]) != 0:
+            writeLock.acquire()
+            container["column"] = "genome"
+            print (json.dumps(container))
+            writeLock.release()
             
-            if len(container["cazy"]) != 0:
-                writeLock.acquire()
-                container["column"] = "genome"
-                print (json.dumps(container))
-                writeLock.release()
-            
-        except Exception:
-            pass
+
 
         
         in_queue.task_done()
@@ -87,6 +86,7 @@ for root in roots.keys():
     for root_initial in root_initials:
         genomes = [y for y in re.findall(roots[root] + "\\d+.html", requests.get(root_initial).content.decode('iso8859-1'))]
         for genome in genomes:
+
             in_queue.put(genome)
             
 in_queue.join()
